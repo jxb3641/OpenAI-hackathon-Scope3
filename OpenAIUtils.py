d9feed265c36f62298ee62389132e2233e466536
Juan Batista <juanbtbx198@aol.com>
2022-11-11 00:21:31 -0500

    added completions to embedding model name dict; removed doc_similarity_search because you would use text-similarity endpoint instead of text-search endpoints

--- OpenAIUtils.py
+++ OpenAIUtils.py
@@ -10,33 +10,38 @@ tokenizer = GPT2TokenizerFast.from_pretrained("gpt2")
 # Keep these here so that we can simply refer to the model family when using the API.
 EMBEDDING_MODELS = {"ada": {"query": "text-search-ada-query-001",
                             "doc": "text-search-ada-doc-001",
+                            "completion": "text-ada-001"
                             },
                     "babbage": {"query": "text-search-babbage-query-001",
                                 "doc": "text-search-babbage-doc-001",
+                                "completion": "text-babbage-001"
                                 },
                     "curie": {"query": "text-search-curie-query-001",
                               "doc": "text-search-curie-doc-001",
+                              "completion": "text-curie-001"
                               },
                     "davinci": {"query": "text-search-curie-query-001",
                                 "doc": "text-search-curie-doc-001",
+                                "completion": "text-davinci-002"
                                 }
                     }
 
-def call_openai_api_completion(prompt, model='text-ada-001'):
+def call_openai_api_completion(prompt, model_family='ada'):
     """Send a request to OpenAI's text generation API endpoint,
     with send_prompt and model.
 
     Args:
         prompt (str): The full prompt. 
-        model (str, optional): model to use for generation. Defaults to 'text-ada-001'.
+        model_family (str, optional): model family to use for generation. Can be any of "ada", "babbage", "curie", "davinci". 
+        Defaults to 'ada'.
 
     Returns:
         str: The top scoring autocompletion. 
     """
 
     response = openai.Completion.create(
-      model=model,
-      prompt=full_prompt,
+      model=EMBEDDING_MODELS[model_family]["completion"],
+      prompt=prompt,
       max_tokens=50,
       temperature=0.2,
     )
@@ -59,30 +64,6 @@ def get_embedding(text, model_family="babbage"):
          return np.ndarray([]) # Return an empty array.
     embedding = openai.Embedding.create(input = [text], model=EMBEDDING_MODELS[model_family]["doc"])['data'][0]['embedding']
     return embedding
-
-def doc_similarity_search(embeddings, context,model_family="babbage", n=3, pprint=True):
-    """Search the doc embeddings for the most similar matches with the context.
-
-    Args:
-        embeddings (DataFrame): df containing 'text' field, and its search/doc embeddings.
-        context (str): Context to embed. Uses the 'doc' version of the embedding model.
-        model_family (str, optional): model family to apply.  can be "davinci", "curie", "babbage", "ada"; Default "babbage"
-        n (int, optional): number of top results. Defaults to 3.
-        pprint (bool, optional): Whether to print the text and scores of the top results. Defaults to True.
-
-    Returns:
-       DataFrame: Top n rows of the embeddings DataFrame, with similarity column added. Sorted by similarity score from highest to lowest. 
-    """
-    embedded = get_embedding(context,EMBEDDING_MODELS[model_family]['doc'])
-    embeddings["similarities"] = embeddings["doc_embeddings"].apply(lambda x: cosine_similarity(x, embedded))
-
-    res = embeddings.sort_values("similarities", ascending=False).head(n)
-    if pprint:
-        for _, series in res.iterrows():
-            print(series["similarities"],series["text"])
-            print()
-    return res
-
 
 def query_similarity_search(embeddings, query, model_family="babbage", n=3, pprint=True):
     """Search the doc embeddings for the most similar matches with the query.
@@ -120,3 +101,4 @@ def produce_prompt(context, query_text):
 
     return """Given Context:\n\n{context}\n\n{query_text}""".format(context,query_text)
 
+#TODO: Add caching/saving functionality to the embeddings calls (or maybe these should be done separately in scripts that use these functions?)
